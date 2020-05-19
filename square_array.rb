3ce9c147c94f34f4e56997b36084634319809f5f
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-19 03:51:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
-  array.each do |element|
-    element = element * element
-    new_array.append(element)
+  array.each do |number|
+    number = number * number
+    new_array.append(number)
   end
   return new_array
 end
